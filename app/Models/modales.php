c848df97cb8a0a0f5a253857ec911002e381e36a
Ant-DeLaT <186017554+Ant-DeLaT@users.noreply.github.com>
2025-01-24 10:51:25 +0100

    First upload of project

---
+++ app/Models/modales.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Models;
+use CodeIgniter\Model;
+class UserModel extends Model{
+    // Cambiar group_name con 
+    protected $DBGroup='group_name';
+    protected function initialize(){
+
+    }
+}
